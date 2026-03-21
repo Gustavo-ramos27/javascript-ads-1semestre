f02cdd82b3276166d7bf3c307c6752cde1dd6ee7
Gustavo Ramos <gustatit20@gmail.com>
2026-03-20 22:46:01 -0300

    Update ex28.js

--- lista3/ex28.js
+++ lista3/ex28.js
@@ -3,4 +3,4 @@ let gorjeta = parseFloat(prompt("Digite a porsentagem da gorjeta:"));
 let persentoal = gorjeta/100
 let valorg = conta * persentoal
 let total = valorg + conta
-document.getElementById('cont').innerHTML = `o valor da conta ${conta}% e o persentoal da gorjeta é ${gorjeta} e o total da gorjeta é ${valorg} e o todo é ${total}`
+document.getElementById('cont').innerHTML = `o valor da conta ${conta} e o persentoal da gorjeta é ${gorjeta}% e o total da gorjeta é ${valorg} e o todo é ${total}`
