a6038516a84eb82e82f25c2297595c76892f8bff
Gustavo Ramos <gustatit20@gmail.com>
2026-03-28 22:13:48 -0300

    2026_03_28

---
+++ lista4/ex35.js
@@ -0,0 +1,4 @@
+let raio = parseFloat(prompt('digite o valor do raio'))
+let area = Math.PI * Math.pow(raio, area)
+let circuferencia = 2 * Math.PI * raio 
+document.getElementById('').innerHTML = `o valor do raio é ${raio} e sua area é ${area} e sua circuferencia é ${circuferencia}`
