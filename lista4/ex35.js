9d5e2448bbb42a8b2c7afad32dc84f360484c075
Gustavo Ramos <gustatit20@gmail.com>
2026-04-01 23:29:56 -0300

    2026-04-01

--- lista4/ex35.js
+++ lista4/ex35.js
@@ -1,4 +1,4 @@
 let raio = parseFloat(prompt('digite o valor do raio'))
 let area = Math.PI * Math.pow(raio, area)
 let circuferencia = 2 * Math.PI * raio 
-document.getElementById('').innerHTML = `o valor do raio é ${raio} e sua area é ${area} e sua circuferencia é ${circuferencia}`
+document.getElementById('resultado').innerHTML = `o valor do raio é ${raio} e sua area é ${area} e sua circuferencia é ${circuferencia}`
--- lista4/ex37.js
+++ lista4/ex37.js
@@ -3,4 +3,4 @@ let base2 = parseFloat(prompt('digite a segunda base'))
 let altura = parseFloat(prompt('digite a altura'))
 let somb = base1 + base2
 let area = somb * altura / 2 
-document.getElementById('resultado ').innerHTML = `o valor area do trapesio é ${area}`
+document.getElementById('resultado').innerHTML = `o valor area do trapesio é ${area}`
