84893daccce75c2f94a9316cef57b1f70010523c
Gustavo Ramos <gustatit20@gmail.com>
2026-04-20 10:52:57 -0300

    Update ex1.js

--- lista_blackBox/ex1.js
+++ lista_blackBox/ex1.js
@@ -1,9 +1,11 @@
 // Exercício 1: Soma dos elementos de uma lista
 // TODO: Crie array com 5 números (prompt), some e mostre
 
-let lista = [1,2,3,4,5]; // preencha com 5 números
-let soma = 0;
-// seu código aqui...
-
+let lista = [];
+for(let i = 0; i < 5; i++) {
+    let num = parseFloat(prompt(`Digite o número ${i+1}:`));
+    lista.push(num);
+}
+let soma = lista.reduce((acc, num) => acc + num, 0);
 document.getElementById('resultado').innerHTML = `Soma da lista: ${soma}`;
 console.log(lista, soma);
