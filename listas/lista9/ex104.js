5124021754b77b3128244fca818cbe76646ffb03
Gustavo Ramos <gustatit20@gmail.com>
2026-05-21 20:32:45 -0300

    Update ex104.js

--- listas/lista9/ex104.js
+++ listas/lista9/ex104.js
@@ -1,2 +1,13 @@
-// vazio
-
+let lista = ["elefante", "borboleta", "chocolate", "tartaruga", "biblioteca", "computador", "dinossauro", "quadro",'sera']
+let maior = ''
+let menor = lista[1]
+for(let i = 0; i < lista.length ; i++){
+    if(lista[i].length > maior.length){
+        maior = lista [i]
+    }
+    if(lista[i].length < menor.length){
+        menor = lista[i]
+    }
+}
+console.log(maior)
+console.log(menor)
