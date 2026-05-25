0abf1d055418159759ddb92c24d5143ce508ac9f
Gustavo Ramos <gustatit20@gmail.com>
2026-05-25 11:30:39 -0300

    2026/05/25

--- listas/lista9/ex113.js
+++ listas/lista9/ex113.js
@@ -1,2 +1,10 @@
-// vazio
+let cursos = ["Tecnologia Informação", "Ciência Dados", "Desenvolvimento Web"]
 
+for(let i = 0 ; i < cursos.length; i++){
+    let sigla = ''
+    let palavras = cursos[i].split(' ')
+for(let j = 0 ; j < palavras.length ; j++){
+    let palavras2 = palavras[j]
+        sigla += palavras2[0]
+}console.log(sigla)
+}
--- listas/lista9/ex114.js
+++ listas/lista9/ex114.js
@@ -1,2 +1,8 @@
-// vazio
-
+let lista = ["cachorro", "borboleta", "arvore", "bicicleta", "montanha", "estrela", "janela", "foguete", "nuvem", "espelho",'ceu']
+let selecionadas = []
+for(let i = 0 ; i<lista.length ; i++){
+    if(lista[i].length >= 4){
+        selecionadas.push(lista[i])
+    }
+    console.log(selecionadas)
+}
